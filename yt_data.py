079f3c5e86f572effc7f5b6f55656f53c952d690
buğra uysal <uysalbugra134@gmail.com>
2025-05-23 03:40:57 +0300

    YouTube API data from many categories was pulled at the same time and saved to an excel file.

--- yt_data.py
+++ yt_data.py
@@ -6,55 +6,46 @@ apı='AIzaSyAW0yub2ubO2LJyUyAyts1--47tb2Um_2k'
 # youtube api servisini başlatma 
 youtube=build('youtube', "v3", developerKey=apı)
 
-catogori= {
-    "10": "Music",
-    "17": "Sports",
-    "20": "Gaming",
-    "24": "Entertainment",
-    "26": "Howto & Style"
-}
-with pd.ExcelWriter("youtube_cato.xlsx", engine="openpyxl")as writer:
+catogori=[('10','Music'),('17','Sports'),('20','Gaming'),('24','Entertainment')]
 
-  for cato_id ,cat in catogori.items():
-  
-     reques=youtube.videos().list(
-         part="snippet,statistics",
-         chart="mostPopular",
-         regionCode="TR",
-         videoCategoryId=cato_id,
-         maxResults=30
-     )
-
-response=reques.execute()
 yt=[]
-print("video başlıkları")
-for video in response['items']:
-    print(video['snippet']['title'])
-    print(video['statistics'])
+""" with pd.ExcelWriter("youtube_data.xlsx", engine='openpyxl')as writer: """
+for cato_id ,cat in catogori:
+       
+       
+       reques=youtube.videos().list(
+           part="snippet,statistics,contentDetails",
+           chart="mostPopular",
+           regionCode="US",
+           videoCategoryId=cato_id,
+           maxResults=50
+       )
+
+       response=reques.execute()
 
 
-    ddvideo={
-        "catogori":cat,
-        "Title":video['snippet']['title'],
-        "Date":video['snippet']['publishedAt'],
-        "liveBroadcastContent":video['snippet']['liveBroadcastContent'],
-        "Channel":video['snippet']['channelTitle'],
+       print("video başlıkları")
+       for video in response['items']:
+           print(video['snippet']['title'])
+           print(video['statistics'])
+       
+           ddvideo={
+               "catogori":cat,
+               "Title":video['snippet']['title'],
+               "Date":video['snippet']['publishedAt'],
+               "Channel":video['snippet']['channelTitle'],
+               "Comment":video['statistics'].get('commentCount', None), 
+               "View":video['statistics'].get('viewCount', None),   
+               "Like":video['statistics'].get('likeCount', None)    ,
+               "Duration":video['contentDetails'].get('duration', None)
+              
+             
 
-
-        "Comment":video['statistics']['commentCount'],
-        "View":video['statistics']['viewCount'],
-        "Like":video['statistics']['likeCount']
-
-   
-    }
-    yt.append(ddvideo)
-    dataf=pd.DataFrame(yt)
+       
+          
+           }
+           yt.append(ddvideo)
+dataf=pd.DataFrame(yt)
      
-    dataf.to_csv("youtube_data.csv", index=True,encoding="utf-8-sig" )
+dataf.to_excel("youtube_data.xlsx", index=True,engine='openpyxl' )
   
-
-
-
-    
-
-
