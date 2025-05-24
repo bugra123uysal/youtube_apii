6496693762739f97b69b4a6be0cb00e816e6a0c5
buğra uysal <uysalbugra134@gmail.com>
2025-05-25 01:36:15 +0300

    new category: Film and Animation and new column: countries: TR,US,DE,GB,IN added

---
+++ data_visualization.sql
@@ -0,0 +1,9 @@
+
+select * from youtubeapii
+select "Title","Comment" from youtubeapii order by "Comment"  desc
+
+select "Title","Comment" from youtubeapii order by "Comment" asc
+
+select "Title","Like" from youtubeapii order by "Like" asc
+select "Title", "View" from youtubeapii  order by "View" asc 
+
--- yt_data.py
+++ yt_data.py
@@ -21,50 +21,57 @@ apı='AIzaSyAW0yub2ubO2LJyUyAyts1--47tb2Um_2k'
 # youtube api servisini başlatma 
 youtube=build('youtube', "v3", developerKey=apı)
 
-catogori=[('2', 'Autos & Vehicles'),
+catogori=[
+     ('1','Film & Animation'),
+     ('2', 'Autos & Vehicles'),
     ('10', 'Music'),
     ('17', 'Sports'),
     ('20', 'Gaming'),
     ('24', 'Entertainment'),
     ('25', 'News & Politics'),
     ('28', 'Science & Technology')]
-
+countrys=['TR','US','GB','IN','DE']
 yt=[]
-for cato_id ,cat in catogori:
-       
-       
-       reques=youtube.videos().list(
-           part="snippet,statistics,contentDetails",
-           chart="mostPopular",
-           regionCode="US",
-           videoCategoryId=cato_id,
-           maxResults=50
-       )
+for coun in countrys:
 
-       response=reques.execute()
-
-
-       print("video başlıkları")
-       for video in response['items']:
-           print(video['snippet']['title'])
-           print(video['statistics'])
-       
-           ddvideo={
-               "catogori":cat,
-               "Title":video['snippet']['title'],
-               "Date":video['snippet']['publishedAt'],
-               "Channel":video['snippet']['channelTitle'],
-               "Comment":video['statistics'].get('commentCount', None), 
-               "View":video['statistics'].get('viewCount', None),   
-               "Like":video['statistics'].get('likeCount', None)    ,
-               "Duration":video['contentDetails'].get('duration', None)
-              
-             
-
-       
           
-           }
-           yt.append(ddvideo)
+    for cato_id ,cat in catogori:
+           
+           
+           reques=youtube.videos().list(
+               part="snippet,statistics,contentDetails",
+               chart="mostPopular",
+               regionCode=coun,
+               videoCategoryId=cato_id,
+               maxResults=50
+           )
+    
+           response=reques.execute()
+    
+    
+           print("video başlıkları")
+           for video in response['items']:
+               print(video['snippet']['title'])
+               print(video['statistics'])
+           
+               ddvideo={
+                   "catogori":cat,
+                   "country":coun,
+                   "Title":video['snippet']['title'],
+                   "Date":video['snippet']['publishedAt'],
+                   "Channel":video['snippet']['channelTitle'],
+                   "Comment":video['statistics'].get('commentCount', None), 
+                   "View":video['statistics'].get('viewCount', None),   
+                   "Like":video['statistics'].get('likeCount', None)    ,
+                   "Duration":video['contentDetails'].get('duration', None),
+                
+                  
+                 
+    
+           
+              
+               }
+               yt.append(ddvideo)
 dataf=pd.DataFrame(yt)
      
 dataf.to_excel("youtube_data.xlsx", index=True,engine='openpyxl' )
