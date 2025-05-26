ab75ae9fbb1279f382419bdf9b77dcbcec14ea05
buğra uysal <uysalbugra134@gmail.com>
2025-05-27 00:00:08 +0300

    I listed the most and least watched videos of certain countries (TR, IN, GB, DE, US) with SQL query.

--- data_visualization.sql
+++ data_visualization.sql
@@ -1,9 +1,23 @@
 
 select * from youtubeapii
+select DISTINCT country from youtubeapii 
+select DISTINCT catogori from youtubeapii
 select "Title","Comment" from youtubeapii order by "Comment"  desc
 
 select "Title","Comment" from youtubeapii order by "Comment" asc
 
-select "Title","Like" from youtubeapii order by "Like" asc
-select "Title", "View" from youtubeapii  order by "View" asc 
+select "Title","Like","View" from youtubeapii order by "Like" asc
+select "Title","Like","View" from youtubeapii order by "Like" desc
 
+select "Title","country","View" from youtubeapii order by "View" asc
+select "Title","country","View" from youtubeapii order by "View" desc
+
+
+select * from youtubeapii
+where country IN ('TR','IN', 'GB', 'DE', 'US')
+order by country, "View" asc
+
+
+select * from youtubeapii
+where country IN ('TR','IN', 'GB', 'DE', 'US')
+order by country, "View" desc
